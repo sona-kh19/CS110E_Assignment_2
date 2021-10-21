54ca007e44fe542a2dd2fe7d882d388287fc2bd3
sona-kh19 <92861506+sona-kh19@users.noreply.github.com>
2021-10-21 10:17:52 +0400

    Update Task_3.js

--- Task_3.js
+++ Task_3.js
@@ -1,3 +1,4 @@
+//Find the length of the line from (x1, y1) to (x2, y2) in the first quarter of x-y axis
 
 const x1 = 1;
 const y1 = 2;
@@ -11,5 +12,4 @@ if (x1 >= 0 && y1 >= 0 && x2 >= 0 && y2 >= 0) {
 } else {
     console.log("The given coordinates are not in the first quarter of x-y axis.");
 }
-
     
